620da90951ff1d1ce6bedfb3bd3d6c5cf97267f5
NumbFish <numbfish@yeah.net>
2019-06-13 15:29:19 +0800

    绿色方块吸附效果

--- Assets/Resources/Constitution/GreenKey/GreenKey.cs
+++ Assets/Resources/Constitution/GreenKey/GreenKey.cs
@@ -7,17 +7,27 @@ public class GreenKey : MonoBehaviour
     float radian = 0; 
     public float radianSpeed = 2f;
     public float radius = 0.2f;
-    Vector3 startPos; 
+    Vector3 startPos;
+    public GameObject player;
+    public float moveSpeed = 0.1f;
+    public float radiusOfAdsorption = 3.0f;
     void Awake()
     {
         gameObject.name = "greenKey";
         gameObject.layer = LayerMask.NameToLayer("interact");
         startPos = transform.position;
+        player = GameObject.Find("nUsr");
     }
     
     void Update() {
         radian += radianSpeed*Time.deltaTime; 
         float dy = Mathf.Cos(radian) * radius;
-        transform.position = startPos + new Vector3(0, dy, 0);
+
+        // 吸附效果
+        if (player != null && Vector2.Distance(transform.position, player.transform.position) < radiusOfAdsorption) {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed);
+        } else {
+            transform.position = startPos + new Vector3(0, dy, 0);
+        }
     }
 }
